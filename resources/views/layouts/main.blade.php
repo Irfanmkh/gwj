c839a447b832c8cf45c23885a37c70846ef1a4b2
Irfan Maulana Khakiki <irfankhakiki17@gmail.com>
2025-04-19 12:46:08 +0700

    add link google fonts

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,8 +16,13 @@
     <title>IRFAN</title>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+    
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Iceberg&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('css')
 </head>
